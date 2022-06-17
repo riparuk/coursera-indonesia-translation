7fb7f4baa7806a92f0320285d8e146d4bd43440b
riparuk <94559656+riparuk@users.noreply.github.com>
2022-06-18 02:05:37 +0700

    Delete background.js

--- background.js
+++
@@ -1,6 +0,0 @@
-// Click the extension icon to send the request to the current tab
-chrome.browserAction.onClicked.addListener(() => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {})
-  })
-})
